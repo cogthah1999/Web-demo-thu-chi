f9960c2c8bd6aa2708a0614ffe25a4c1cb29b141
cogthah1999 <congthanh1999.1@gmail.com>
2026-04-29 11:19:02 +0700

    Connect Frontend to Render Backend

--- my-wallet-frontend/src/App.js
+++ my-wallet-frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
   const [darkMode, setDarkMode] = useState(() => {
     return localStorage.getItem("darkMode") === "true";
   });
-  const API_URL = 'http://localhost:8080/api/payments';
+  const API_URL = 'https://web-demo-thu-chi-my-wallet-api.onrender.com';
   const ADMIN_KEY = 'my-secret-key-123'; // Khớp với Backend
 
   const fetchPayments = () => axios.get(API_URL).then(res => setPayments(res.data));
